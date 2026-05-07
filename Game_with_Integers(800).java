15a1579e2af7d7d5d08854d117dc31e2f6ed95ef
Banala Yaswanth Naidu <yaswanthnaidu717@gmail.com>
2026-05-07 09:34:39 +0530

    Create Game_with_Integers(800).java

---
+++ Game_with_Integers(800).java
@@ -0,0 +1,17 @@
+//If n is divisible by 3, Vova wins otherwise Vanya makes it divisible by 3 in one move and wins
+
+import java.util.*;
+class Codeforces {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while(t-- > 0) {
+            int n = sc.nextInt();
+            if(n % 3 == 0)
+                System.out.println("Second");
+            else
+                System.out.println("First");
+        }
+    }
+}
